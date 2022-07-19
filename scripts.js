46148aa815b5628f618c16dba4b70fcaecd9ef92
behshad <82580731+biigii@users.noreply.github.com>
2022-07-19 21:04:05 +1200

    javascript added

---
+++ scripts.js
@@ -0,0 +1,80 @@
+// alert("this is my first JS statement");
+
+// string
+// console.log('hello world');
+
+// console.log(1000);
+// console.log('1000');
+
+
+// numbers
+// console.log(100 * 100);
+
+// console.log(1000 + 2000);
+// console.log('1000' + 2000);
+// console.log('hello' + ' ' + 'world');
+// console.log('john ' + 'doe');
+
+// boolean true or false
+// console.log(false)
+// console.log('false')
+
+// array
+// ['john','steve','mary']
+// [1, 3, 10, 99, 5, 'abc', true, false, 100, 0, 'hello world']
+
+// javascript object 
+// {
+//   name: 'john',
+//   surname: 'doe',
+//   age: 40
+// }
+
+// snake casing php and python
+// my_family_name
+
+// kebab-casing
+// my-family-name
+
+//title-casing
+// MyFamilyName
+
+// camel casing
+var myName; 
+// var myFamilyName;
+
+myName = 'behshad';
+// console.log(myName)
+
+// var age 
+// age = 100
+// console.log(age)
+// age = 101
+// console.log(age)
+
+// var age;
+// age = 20;
+// console.log(age);
+
+// let age;
+// age = 33;
+// console.log(age);
+// age = 40;
+// console.log(age);
+
+// const age = 50;
+// console.log(age)
+// age = 51;
+
+{
+  let age = 50
+}
+
+
+{
+  let age = 90
+}
+
+// let age = 30;
+// let age = 21;
+
